a5d820af10649ac7a03cb2885cda36df77b0e4c5
Michael Trott <mtrott3000@hotmail.co.uk>
2024-06-09 20:13:05 +0100

    Changed Output type from Console application to Class Library.

--- WordFrequency/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// See https://aka.ms/new-console-template for more information
-using Test;
-
-Console.WriteLine("Hello, World!");
-
-var test = new WordFrequencyAnalyzer();
-Console.WriteLine(test.CalculateMostFrequentWords("The sun shines over the lake", 3).ToString());
